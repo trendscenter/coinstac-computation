40f35ac67e9569d1b08ea8963df4ddf54a65d18f
Aashis Khanal <sraashis@gmail.com>
2021-11-24 20:27:36 -0500

    Call obj.

--- examples/multi_iterations/entry.py
+++ examples/multi_iterations/entry.py
@@ -3,4 +3,4 @@ import coinstac_computation.server as server
 from local_pipeline import local
 from remote_pipeline import remote
 
-server.start(local.compute, remote.compute)
+server.start(local, remote)
