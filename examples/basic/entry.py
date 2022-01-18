8dd265b4f1f7205719e2c0dcb47ca3cc5e0f24ad
Aashis Khanal <sraashis@gmail.com>
2022-01-18 12:33:02 -0500

    Use new coinstac server.

--- examples/basic/entry.py
+++ examples/basic/entry.py
@@ -1,6 +1,6 @@
-import coinstac_computation.server as server
+import coinstac
 
 from local_pipeline import local
 from remote_pipeline import remote
 
-server.start(local, remote)
+coinstac.start(local, remote)
